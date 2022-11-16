9c00a9e7e0fd4dca712e79fa06d1b58ec44885e3
Eugenia Villegas <maria.villegas@accenture.com>
2022-11-16 09:12:40 -0300

    Sin capitalizar

--- 05-app-clima/app.js
+++ 05-app-clima/app.js
@@ -21,17 +21,31 @@ const main = async() => {
                 const places = await searchs.city(place);
                 //Select an option
                 const id = await listPlaces(places);
+                if (id === '0') continue;
+
                 const selectedPlace = places.find(l => l.id === id);
-                // console.log(selectedPlace);
-                
+                //Save selected places
+                searchs.addHistory(selectedPlace.name)
+                //Weather
+                const weather = await searchs.placeWeather(selectedPlace.lat, selectedPlace.lon);
+
                 // console.log('City info');
+                console.clear()
+                console.log('\nCity information:');
                 console.log('City: ', selectedPlace.name);
                 console.log('Latitud: ', selectedPlace.lat);
-                console.log('Longitud: ', selectedPlace.lng);
-                // console.log('Temperature: ');
-                // console.log('Min: ');
-                // console.log('Max: ');
+                console.log('Longitud: ', selectedPlace.lon);
+                console.log('Temperature: ', weather.temp);
+                console.log('Min: ', weather.min);
+                console.log('Max: ', weather.max);
+                console.log('Description: ', weather.desc.green);
                 break;
+            case 2:
+                searchs.history.forEach((place, i) => {
+                    const idx = `${i + 1}.`.green;
+                    console.log(`${idx} ${place}`);
+                })
+            break;
         }
 
         if (opt !== 0) await pausa();
--- 05-app-clima/models/searchs.js
+++ 05-app-clima/models/searchs.js
@@ -1,9 +1,18 @@
+import * as fs from 'fs';
 import axios from "axios";
+import capitalize from 'capitalize';
 class Searchs {
-    // history = ['Mendoza', 'Buenos Aires', 'Cordoba'];
+    history = [];
+    dbPath = './db/database.json'
 
     constructor() {
-        //to do leer db if exists
+        this.readDB();
+    }
+
+    get capitalizedHistory() {
+        return this.history.map(place => {
+            return capitalize.words(place)
+        });
     }
 
     get paramsMapbox() {
@@ -12,7 +21,15 @@ class Searchs {
             'language':'en',
             'access_token': process.env.MAPBOX_KEY,
         }
-    } 
+    }
+    
+    get paramsOpenweather() {
+        return {
+            units: 'metric',
+            language: 'en',
+            appid: process.env.OPENWEATHER_KEY,
+        }
+    }
 
     async city(place = '') {
         
@@ -28,15 +45,66 @@ class Searchs {
             return resp.data.features.map(place => ({
                 id: place.id,
                 name: place.place_name,
-                lng: place.center[0],
+                lon: place.center[0],
                 lat: place.center[1]
             }));
         } catch (error) {
             console.log(error);
             return [];
         }
+    }
 
+    async placeWeather(lat, lon) {
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/`,
+                params: {lat, lon, ...this.paramsOpenweather}
+            });
 
+            const resp = await instance.get(`weather`);
+
+            const {weather, main} = resp.data;
+            
+            return {
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+            
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    addHistory(place = '') {
+        if (this.history.includes(place.toLocaleLowerCase())) {
+            return;
+        }
+
+        this.history = this.history.splice(0, 4)
+
+        this.history.unshift(place.toLocaleLowerCase());
+
+        this.saveDB(); //saving in db
+
+    }
+
+    saveDB() {
+        const payload = {
+            history: this.history
+        }
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload))
+    }
+
+    readDB() {
+        if(!fs.existsSync(this.dbPath)) return;
+
+        const info = fs.readFileSync(this.dbPath, {encoding: 'utf-8'});
+        const data = JSON.parse(info);
+
+        this.history = data.history;
     }
 }
 
